60ef827f23a7cbf5829201ca34e1efcf05e3d8d1
Siddhant Mishra <siddhant48311@gmail.com>
2020-07-22 21:54:25 +0530

    Routing Started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Switch, Route} from "react-router-dom";
 import Homepage from './pages/homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+      <Route exact path='/' component={Homepage} />     </Switch>
     </div>
   );
 }
