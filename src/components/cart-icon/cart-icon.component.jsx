15bc58287b90eac62e9d151a9e9eef2f2ff09519
Siddhant Mishra <siddhant48311@gmail.com>
2020-08-28 06:56:28 +0530

    Item count feature added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,19 +4,21 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({toggleCartHidden, itemCount}) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0) // this is a selector
+}); //but like this the mapStateToProps will we called whenever the state change for anything (eg. user login) and rerender the component so we need to cache the value only only pass it to the component when the particular value changes.
 
 const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()), //here we create a new togglecartfunction which dispatches the functionality of togglecartfunction
+  toggleCartHidden: () => dispatch(toggleCartHidden()), //here we create a new togglecartfunction which dispatches the functionality of toggleCartfunction
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
