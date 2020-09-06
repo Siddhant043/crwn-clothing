7b5847e9ff94d7ca9f40cb450c90fc5f4485c21e
Siddhant Mishra <siddhant48311@gmail.com>
2020-09-07 05:03:25 +0530

    Persist feature added using local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // local storage
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root', // at what point in reducer object do we wanna store anything
+    storage,
+    whitelist: ['cart'] // which reducers you want to persist
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer); // returns modified version of rootReducer
