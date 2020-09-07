ee6ea2a8f503090ab748f9b2b8cdb5eb0a44218e
Siddhant Mishra <siddhant48311@gmail.com>
2020-09-07 11:07:14 +0530

    Collections overview component created

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+import "./collection-overview.styles.scss";
+
+export const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,13 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import "./shop.styles.scss";
 
-
-
-export const ShopPage = ({collections}) => {
+export const ShopPage = ({ collections }) => {
   return (
     <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-  )
-}
+      <CollectionsOverview />
+    </div>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-})
-
-const mapDispatchToProps = {
-  
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage)
+export default ShopPage;
