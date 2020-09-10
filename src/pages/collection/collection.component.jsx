e31f3e18eb29ef3743ab463f1ef428040c0dc15e
Siddhant Mishra <siddhant48311@gmail.com>
2020-09-10 10:48:39 +0530

    Collection item presentation on collection page and directory routing to collection page feature added

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,18 +4,25 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-    console.log(collection);
+const CollectionPage = ({ collection }) => { // collection is our main item returned from the selecto eg. hats jackets
+    const {title, items} = collection;
     return (
-        <div className='collection-page'>
-            <h2>Collection Page</h2>
+        <div className='collection-page '>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+             {
+                 items.map(item => (
+                     <CollectionItem key={item.id} item={item} />
+                 ))
+             }
+            </div>
         </div>
     );
 }
 
 const mapStateToProps = (state, ownProps) => ({ // ownProps contain all the props which were given to the collection page component
     collection: selectCollection(ownProps.match.params.collectionId)(state),
-    // this is nessary because unlike other selectors, this one needs apart of the state depending on the URL parameter.
+    // this is nessary because unlike other selectors, this one needs a part of the state depending on the URL parameter.
 })
 
 export default connect(mapStateToProps)(CollectionPage);
