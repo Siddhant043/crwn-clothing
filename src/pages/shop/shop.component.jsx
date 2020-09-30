1969998a28ee5aa7758b3b97a60a52c4e06fd1f2
Siddhant Mishra <siddhant48311@gmail.com>
2020-09-30 23:40:53 +0530

    used promise in shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,7 +27,7 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then(async (snapshot) => {
       const collectionsMap = converCollectionSnapshotToMap(snapshot);
       updateCollections(collectionsMap); // passing collections to our component
       this.setState({ loading: false });
